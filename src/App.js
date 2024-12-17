27490df54e72ec5ca7fbd283fb85d895b370ebad
shahoul95 <shahoul@libheros.fr>
2024-12-17 10:41:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "./containers/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Bienvenue sur mon application React</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/containers/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer style={{ marginTop: "20px", textAlign: "center" }}>
+      <p>&copy; 2024 - Mon Application React</p>
+    </footer>
+  );
+};
+
+export default Footer;
